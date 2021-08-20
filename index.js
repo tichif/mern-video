15b747158673233c4974bf49ec0b497ece540954
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-08-19 22:06:14 -0400

    Setup the socket

--- index.js
+++ index.js
@@ -16,4 +16,23 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  // broadcast a message when the connection is disconnected
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callended');
+  });
+
+  socket.on('calluser', (data) => {
+    const { userToCall, signalData, from, name } = data;
+
+    io.to(userToCall).emit('calluser', { signal: signalData, from, name });
+  });
+
+  socket.on('answercall', (data) => {
+    io.to(data.to).emit('callaccepted', data.signal);
+  });
+});
+
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
