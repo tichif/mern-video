9844e6576ff679cb60989d0cb9abb5bc170290b5
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-08-19 22:39:04 -0400

    Create the layout for the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
+import { Typography, AppBar } from '@material-ui/core';
+
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
 }
 
 export default App;
