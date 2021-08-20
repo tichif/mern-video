c80ff07108feb364fdea10700f4356fc3c0664b4
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-08-19 23:21:25 -0400

    Import the context into the index.js file (client)

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import './styles.css';
+import { ContextProvider } from './SocketContext';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>,
+  </ContextProvider>,
   document.getElementById('root')
 );
